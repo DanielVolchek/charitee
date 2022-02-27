3b3d252927be414fa5bd147e0fa89641613fef3a
Deland <chen.deland@gmail.com>
2022-02-27 08:53:05 -0500

    notif button

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -10,15 +10,15 @@ if (checkSignedIn()) {
     document.getElementById("signin").setAttribute("class", "hide")
     document.getElementById("signup").setAttribute("class", "hide")
     const notifButton = document.createElement("button")
-    notifButton.setAttribute("class", "notification-button")
+    notifButton.setAttribute("id", "notification-button")
     notifButton.addEventListener("click", (event) => {
         console.log(window.location.href)
         const href = window.location.href
-        for (let i = 0; i < href.length; i++){
-           if (href.substring(i, i+5) === "index"){
+        for (let i = 0; i < href.length; i++) {
+            if (href.substring(i, i + 5) === "index") {
                 window.location.href = href.substring(0, i) + "html/notificationpage.html"
                 return
-           }
+            }
         }
     })
     document.getElementById("account-links").appendChild(notifButton)
