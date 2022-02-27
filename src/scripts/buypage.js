b9d2778be74a0348e411a712ae5fa75f8aba13be
danielvolchek <danielvolchek123@gmail.com>
2022-02-27 04:53:15 -0500

    tag search

--- src/scripts/buypage.js
+++ src/scripts/buypage.js
@@ -18,12 +18,12 @@ console.dir(window)
 console.log(window.location.href)
 const app = initializeApp(firebaseConfig);
 const db = getFirestore();
-
 const imageStore = getStorage()
 const data = collection(db, "donations")
 const q = query(data)
 const querySnapshot = await getDocs(q);
 
+let searchTags = []
 const postContainer = document.getElementById("items-container")
 let subURL = window.location.href
 for (let i = 0; i < subURL.length; i++) {
@@ -35,20 +35,71 @@ for (let i = 0; i < subURL.length; i++) {
     }
 }
 console.log(subURL)
-const imgURLS = []
-querySnapshot.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
-    const docData = doc.data()
-    const pathRef = ref(imageStore, `gs://charitee-e8cba.appspot.com/images/${docData.images[0]}`)
-    getDownloadURL(pathRef).then((url) => {
-        const newPost = document.createElement('img')
-        newPost.addEventListener("click", (event) => {
-            window.location.href = `${subURL}item.html?id=${docData.id}`
+const queryLoop = (querySnap => {
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        const docData = doc.data()
+        const pathRef = ref(imageStore, `gs://charitee-e8cba.appspot.com/images/${docData.images[0]}`)
+        const tags = docData.tags
+        while (postContainer.firstChild) {
+            postContainer.removeChild(postContainer.firstChild);
+        }
+        getDownloadURL(pathRef).then((url) => {
+            let addValue = false
+            if (searchTags.length === 0) {
+                addValue = true
+            }
+            else {
+                for (let t of searchTags) {
+                    if (tags.includes(t)) {
+                        addValue = true
+                        break
+                    }
+                }
+            }
+            console.log(addValue)
+            if (addValue) {
+                const newPost = document.createElement('img')
+                newPost.addEventListener("click", (event) => {
+                    window.location.href = `${subURL}item.html?id=${docData.id}`
+                })
+                newPost.setAttribute('src', url)
+                newPost.setAttribute('class', 'item')
+                postContainer.appendChild(newPost)
+            }
         })
-        newPost.setAttribute('src', url)
-        newPost.setAttribute('class', 'item')
-        postContainer.appendChild(newPost)
-    })
-    console.log(doc.id, " => ", doc.data());
-});
+    });
+})
+queryLoop(querySnapshot)
 
+const modifySearch = (event) => {
+    event.preventDefault()
+    console.dir(event)
+    const input = event.target.children[0]
+    const tags = input.value
+    const letterCheck = /^[a-zA-Z]+$/g;
+    let lastWordIndex = 0
+    searchTags = []
+    for (let i = 0; i < tags.length; i++) {
+        if (tags[i] === " ") {
+            const add = tags.substring(lastWordIndex, i)
+            console.log("regex test")
+            console.log(letterCheck.test(add))
+            console.log(letterCheck.test(add))
+            if (letterCheck.test(add)) {
+                // regex bug only works every other check in loop
+                letterCheck.test(add)
+                searchTags.push(add)
+            }
+            lastWordIndex = i + 1
+        }
+    }
+    const add = tags.substring(lastWordIndex)
+    if (letterCheck.test(add)) {
+        searchTags.push(add)
+    }
+    event.target.children[0].value = ""
+    queryLoop(querySnapshot)
+}
+
+document.getElementById("searchForm").addEventListener("submit", modifySearch)
